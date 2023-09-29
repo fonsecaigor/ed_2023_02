2c50dc74533a5d2de587292443410b2a54b92bbc
Igor Fonseca <fonsecaigr@gmail.com>
2023-09-29 19:53:29 -0300

    correção

--- Introduction/p2_8_if.ts
+++ Introduction/p2_8_if.ts
@@ -1,25 +1,32 @@
 class MyArray {
-    private elements: number[];
+  private elements: number[];
 
-    constructor(elements: number[]) {
-        this.elements = elements;
-    }
+  constructor(elements: number[]) {
+    this.elements = elements;
+  }
 
-    imprimirRecursivo(i: number = 0): void {
-        if (i < this.elements.length) {
-            console.log(this.elements[i]);
-            this.imprimirRecursivo(i + 1);
-        }
+  imprimirRecursivo(i: number = 0): void {
+    // Verifica se o índice está dentro dos limites do array
+    if (i < this.elements.length) {
+      // Imprime o elemento atual
+      console.log(this.elements[i]);
+      // Chama recursivamente a função para o próximo elemento
+      this.imprimirRecursivo(i + 1);
     }
+  }
 
-    imprimirReverso(i: number = this.elements.length - 1): void {
-        if (i >= 0) {
-            console.log(this.elements[i]);
-            this.imprimirReverso(i - 1);
-        }
+  imprimirReverso(i: number = this.elements.length - 1): void {
+    // Verifica se o índice está dentro dos limites do array
+    if (i >= 0) {
+      // Retorna o elemento atual e chama recursivamente para o elemento anterior
+      console.log(this.elements[i]);
+      this.imprimirReverso(i - 1);
+      // Quando o índice se torna negativo, todos os elementos foram processados
     }
+  }
 }
 
+// Exemplo de uso da classe MyArray com um array de números
 const elementos = [1, 2, 3, 4, 5];
 const myArray = new MyArray(elementos);
 
